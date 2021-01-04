3f116f0091d30190e24bac335384ab003778983a
Zora Santos <zoranildo22@gmail.com>
2021-01-04 19:47:26 -0300

    first commit

---
+++ tasks/backend/api/auth.js
@@ -0,0 +1,25 @@
+const { authSecret } = require('../.env')
+const jwt = require('jwt-simple')
+const bcrypt = require('bcrypt-nodejs')
+
+module.exports = app => {
+  const signin = async (req, res) => {
+    if(!req.body.email || !req.body.password) {
+      return res.status(400).send('Dados incompletos!')
+    }
+
+    const user = await app.db('users')
+      .where({ email: req.body.email })
+      .first()
+    
+    if(user) {
+      bcrypt.compare(req.body.password, user.password, (err, isMatch) => {
+        if(err || !isMatch) {
+          return res.status(401).send()
+        }
+
+        const payload =
+      })
+    }
+  }
+}
